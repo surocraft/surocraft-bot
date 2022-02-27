3d4288d8e05eb0e810063feb4e35f9d870841aa9
PetyXbronCZ <coolpetr10@gmail.com>
2022-02-27 16:09:57 +0100

    Edited vote notification message

--- commands/pushVote.js
+++ commands/pushVote.js
@@ -26,7 +26,16 @@ module.exports.run = async (bot, message, args) => {
     );
 
 
-  const msg = await message.reply({ content: `Jsi si jistý, že chceš odeslat notifikaci k hlasování do <#921803832667832380> a zmínit roli?`, components: [row] });
+  const msg1 = await message.reply({ content: `Jsi si jistý, že chceš odeslat notifikaci k hlasování do <#921803832667832380> a zmínit roli?`, components: [row] });
+
+  const votePingEmbedNoMention = new MessageEmbed()
+        .setAuthor({ name: config.server.name ? config.server.name : bot.channels.cache.get('812280438490923048').name, iconURL: server.icon ? server.icon : bot.channels.cache.get('812280438490923048').icon })
+        .setTitle("Je čas hlasovat! 🔔")
+        .setDescription("*Právě je 17:00*\n**Hlasovat můžeš na:**\nHlavní stránce **__[zde](https://minecraftpocket-servers.com/server/113005/vote)__**\nDruhé stránce **__[zde](https://minecraftpocket-servers.com/server/113005/vote)__** (získáš 1K navíc)\n\nVíce o hlasování najdeš na __[wiki](https://wiki.surocraft.eu/#vote)__.\nNastav si připomínaček k hlasování __[zde](https://discord.com/channels/812280438490923048/870356969595228170/921812083916550214)__!")
+        .setFooter({ text: 'Made by PetyXbron', iconURL: 'https://i.imgur.com/oq70O0t.png' })
+        .setColor(config.embeds.color);
+  const msg2 = await message.channel.send({ content: "*Náhled notifikace:*"})
+  const msg3 = await message.channel.send({ content: `@MENTION\nhttps://bit.ly/surocraft-vote`, embeds: [votePingEmbedNoMention] });
 
   const filter = i => i.customId === 'pushVote' && i.user.id === message.author.id;
 
@@ -36,20 +45,24 @@ module.exports.run = async (bot, message, args) => {
 
   collector.on('collect', async i => {
     if (i.customId === 'pushVote') {
+      msg2.delete()
+      msg3.delete()
       const votePingChannel = bot.channels.cache.get('921803832667832380');
       const votePingEmbed = new MessageEmbed()
         .setAuthor({ name: config.server.name ? config.server.name : bot.channels.cache.get('812280438490923048').name, iconURL: server.icon ? server.icon : bot.channels.cache.get('812280438490923048').icon })
         .setTitle("Je čas hlasovat! 🔔")
-        .setDescription("*Právě je 17:00*\n**Hlasovat můžeš __[zde](https://minecraftpocket-servers.com/server/113005/vote)__** 📬\n\nVíce o hlasování najdeš na __[wiki](https://wiki.surocraft.eu/#vote)__.\nNastav si připomínaček k hlasování __[zde](https://discord.com/channels/812280438490923048/870356969595228170/921812083916550214)__!")
+        .setDescription("*Právě je 17:00*\n**Hlasovat můžeš na:**\nHlavní stránce **__[zde](https://minecraftpocket-servers.com/server/113005/vote)__**\nDruhé stránce **__[zde](https://minecraftpocket-servers.com/server/113005/vote)__** (získáš 1K navíc)\n\nVíce o hlasování najdeš na __[wiki](https://wiki.surocraft.eu/#vote)__.\nNastav si připomínaček k hlasování __[zde](https://discord.com/channels/812280438490923048/870356969595228170/921812083916550214)__!")
         .setFooter({ text: 'Made by PetyXbron', iconURL: 'https://i.imgur.com/oq70O0t.png' })
         .setColor(config.embeds.color);
       votePingChannel.send({ content: `<@&932655587861364776>\nhttps://bit.ly/surocraft-vote`, embeds: [votePingEmbed] });
 
-      await msg.edit({ content: `Notifikace odeslána!`, components: [] });
+      await msg1.edit({ content: `Notifikace odeslána!`, components: [] });
     }
   });
 
   collector.on('error', async () => {
+    msg2.delete()
+    msg3.delete()
     const newRow = new MessageActionRow()
       .addComponents(
         new MessageButton()
@@ -59,6 +72,21 @@ module.exports.run = async (bot, message, args) => {
           .setEmoji('✉️')
           .setDisabled(true),
       );
-    await msg.edit({ content: `Čas vypršel.\nNotifikace nebyla odeslána!`, components: [newRow] });
+    await msg1.edit({ content: `Čas vypršel.\nNotifikace nebyla odeslána!`, components: [newRow] });
+  });
+
+  collector.on('end', async () => {
+    msg2.delete()
+    msg3.delete()
+    const newRow = new MessageActionRow()
+      .addComponents(
+        new MessageButton()
+          .setCustomId('pushVote')
+          .setLabel('Odeslat')
+          .setStyle('DANGER')
+          .setEmoji('✉️')
+          .setDisabled(true),
+      );
+    await msg1.edit({ content: `Čas vypršel.\nNotifikace nebyla odeslána!`, components: [newRow] });
   });
 };
